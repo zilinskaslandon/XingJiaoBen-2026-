03b55ddca3645cc5a962c8206843f1eb9327f9a9
Laurenbabey <zilinskaslandon@gmail.com>
2026-08-05 15:47:37 +0800

    Add files via upload

--- 星脚本0.0版本.lua
+++ 星脚本0.0版本.lua
@@ -1825,14 +1825,14 @@ about5:Button({
     end
 })
 
-about5:Paragraph({ Title = "Q - 靠近" })
-about5:Paragraph({ Title = "E - 离远" })
-about5:Paragraph({ Title = "Y - 投掷" })
-about5:Paragraph({ Title = "J - 超级投掷" })
-about5:Paragraph({ Title = "U - 使物体自转" })
-about5:Paragraph({ Title = "P - 使物体悬浮在空中" })
-about5:Paragraph({ Title = "X - 走得更远一点" })
-about5:Paragraph({ Title = "L - 使方块变直并锁定在前部" })
+about5:Label({ Title = "Q - 靠近" })
+about5:Label({ Title = "E - 离远" })
+about5:Label({ Title = "Y - 投掷" })
+about5:Label({ Title = "J - 超级投掷" })
+about5:Label({ Title = "U - 使物体自转" })
+about5:Label({ Title = "P - 使物体悬浮在空中" })
+about5:Label({ Title = "X - 走得更远一点" })
+about5:Label({ Title = "L - 使方块变直并锁定在前部" })
 
 about5:Button({
     Title = "让手上的道具飘起来",
@@ -1841,75 +1841,6 @@ about5:Button({
     end
 })
 
-about5:Paragraph({ Title = "J - 飞起来" })
-about5:Paragraph({ Title = "K - 回到手中" })
+about5:Label({ Title = "J - 飞起来" })
+about5:Label({ Title = "K - 回到手中" })
 
-local about6 = MainSection:Tab({ 
-       Title = "开发工具", 
-       Icon = "rbxassetid://18941716391",
-       PremiumOnly = true
-})
-
-about6:Button({
-    Title = "控制台",
-    Callback = function()
-        game:GetService("StarterGui"):SetCore("DevConsoleVisible", true)
-    end
-})
-
-about6:Button({
-    Title = "汉化版Dex",
-    Callback = function()
-        loadstring(game:HttpGet("https://gitee.com/cmbhbh/cmbh/raw/master/Bex.lua"))()
-    end
-})
-
-about6:Button({
-    Title = "DEX-ExplorerV1 Mobile",
-    Callback = function()
-        getgenv().Key = "Bash"
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/crceck123/roblox-script/main/MC_IY%20Dex.txt"))()
-    end
-})
-
-about6:Button({
-    Title = "DEX-ExplorerV1 Mobile",
-    Callback = function()
-        loadstring(game:HttpGet("https://github.com/XiaoYunCN/VIP/raw/main/DEX-Explorer_Mobile.lua", true))()
-    end
-})
-
-about6:Button({
-    Title = "DEX-ExplorerV2",
-    Callback = function()
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/IlIlIlIlIlIlIlIlIlIlIlIlIlIlIlIl/Script/main/Tools/Dex-ExplorerV2.lua", true))()
-    end
-})
-
-about6:Button({
-    Title = "DEX-V3",
-    Callback = function()
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/IlIlIlIlIlIlIlIlIlIlIlIlIlIlIlIl/Script/main/Tools/DarkDexV3.lua", true))()
-    end
-})
-
-about6:Button({
-    Title = "SimpleSpy",
-    Callback = function()
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/IlIlIlIlIlIlIlIlIlIlIlIlIlIlIlIl/Script/main/Tools/SimpleSpyMobile.lua", true))()
-    end
-})
-
-about6:Button({
-    Title = "FrostHook_Spy",
-    Callback = function()
-        loadstring(game:HttpGet("https://github.com/Nootchtai/FrostHook_Spy/raw/master/Spy.lua", true))()
-    end
-})
-
-about6:Button({
-    Title = "WebHook工具",
-    Callback = function()
-        loadstring(game:HttpGet("https://github.com/XiaoYunUwU/XiaoYunUwU/raw/main/WebhookTool", true))()
-    end
-})
